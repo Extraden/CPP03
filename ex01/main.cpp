0d8b6a16232f6c1bd8dfba9fe45e471464b11a5f
dsemenov <dsemenov@student.42lyon.fr>
2026-02-21 17:42:14 +0100

    feat: add virtual for ClavTrap destructor

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -8,6 +8,14 @@ int	main()
   ScavTrap a;
 
   a.attack("enemy");
+  a.beRepaired(15);
+  a.takeDamage(4);
+  a.takeDamage(20);
 
+  ClapTrap(b);
+
+  b.attack("somebody");
+  b.takeDamage(200);
+  b.beRepaired(10);
 	return (0);
 }
